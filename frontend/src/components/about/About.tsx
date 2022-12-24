45d76452fe42abdb12c20dbe86ca5a495b232508
Naqi Haider <naqihaider07@gmail.com>
2022-12-23 19:38:25 -0500

    updated responsiveness

--- frontend/src/components/about/About.tsx
+++ frontend/src/components/about/About.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React from "react";
 import Image from "next/image";
 
 export default function About() {
   return (
-    <div className="bg-gradient-to-br from-yellow-900 to-amber-300 dark:bg-gradient-to-br dark:from-blue-900 dark:to-indigo-200 w-fit h-fit rounded-xl">
-    <Image
-      className="rounded-xl mix-blend-overlay hover:mix-blend-normal"
-      src="/me.jpg"
-      width={300}
-      height={300}
-      alt="Image of Naqi"
-    ></Image>
-  </div>
-  )
+    <div className="flex justify-center items-center md:min-h-screen">
+
+      <section className="flex flex-col grow md:py-16 md:mx-40 gap-8">
+        <h2 className="text-5xl self-center md:text-6xl lg:text-7xl">About</h2>
+
+        <div className="flex flex-col gap-8 justify-center items-center">
+          <div className="bg-gradient-to-br from-yellow-900 to-amber-300 dark:bg-gradient-to-br dark:from-blue-900 dark:to-indigo-200 rounded-3xl md:rounded-full max-w-lg max-h-lg">
+            <img
+              className="rounded-3xl md:rounded-full mix-blend-overlay hover:mix-blend-normal object-cover aspect-ratio"
+              src="/me.jpg"
+              alt="Image of Naqi"
+            />
+          </div>
+
+          <div>
+            <p className="text-justify md:text-lg max-w-xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum
+              dolor sit amet consectetur adipiscing elit. Iaculis nunc sed augue
+              lacus viverra vitae. Risus nec feugiat in fermentum posuere urna
+              nec tincidunt praesent. Venenatis a condimentum vitae sapien.
+              Egestas erat imperdiet sed euismod nisi porta lorem mollis.
+            </p>
+          </div>
+        </div>
+      </section>
+
+    </div>
+  );
 }
--- frontend/src/components/work/Work.tsx
+++ frontend/src/components/work/Work.tsx
@@ -2,8 +2,32 @@ import React from "react";
 
 export default function Work() {
   return (
-    <>
-        Work
-    </>
+    <div className="flex justify-center items-center md:min-h-screen">
+      <section className="flex flex-col grow md:py-16 md:mx-40 gap-8 justify-center items-center">
+        <h2 className="text-5xl self-center md:text-6xl lg:text-7xl">My Experience.</h2>
+        <div>
+          <h6 className="text"></h6>
+          <p className="text-justify md:text-lg max-w-xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Est velit egestas dui
+            id ornare arcu. Ullamcorper morbi tincidunt ornare massa eget egestas.
+            Magna etiam tempor orci eu. Congue quisque egestas diam in arcu cursus
+            euismod quis. Laoreet sit amet cursus sit. Cursus in hac habitasse platea
+            dictumst quisque sagittis. Ornare quam viverra orci sagittis eu volutpat
+            odio. Diam quam nulla porttitor massa id neque aliquam. At varius vel
+            pharetra vel turpis nunc eget. Neque egestas congue quisque egestas diam
+            in arcu cursus. Curabitur gravida arcu ac tortor. Semper risus in
+            hendrerit gravida rutrum quisque. Nulla facilisi etiam dignissim diam quis
+            enim. Ac ut consequat semper viverra. Sed tempus urna et pharetra. Viverra
+            tellus in hac habitasse platea dictumst. Fermentum leo vel orci porta non
+            pulvinar neque. Commodo odio aenean sed adipiscing diam. Et odio
+            pellentesque diam volutpat commodo sed. Libero nunc consequat interdum
+            varius sit amet. Nisl pretium fusce id velit ut tortor pretium viverra.
+            Turpis tincidunt id aliquet risus feugiat in anteW.
+          </p>
+
+        </div>
+      </section>
+    </div>
   );
 }
