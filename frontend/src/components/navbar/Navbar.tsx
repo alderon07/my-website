e3d00bbd16b050804913e09e055d9f7809e2a818
Naqi Haider <naqihaider07@gmail.com>
2022-12-23 22:21:39 -0500

    Added Logo

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -10,13 +10,16 @@ type LayoutProps = { children: ReactNode };
 export default function Navbar() {
   const { theme, setTheme } = useTheme();
 
-  const style ="ease-in-out delay-100 duration-300 hover:text-amber-500 dark:hover:text-sky-300 transition-all";
+  const style =
+    "ease-in-out delay-100 duration-300 hover:text-amber-500 dark:hover:text-sky-300 transition-all";
 
   return (
     <div>
       <nav className="flex justify-between items-center text-lg lowercase h-20 rounded">
         <span>
-          <Link href="/">naqi</Link>
+          <Link href="/">
+            <img className={`w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 ${style} hover:scale-105`} src="logo.svg" alt="logo" />
+          </Link>
         </span>
         <span>
           <ul className="flex list-none gap-4 items-center">
@@ -25,10 +28,10 @@ export default function Navbar() {
             </li>
 
             <Socials style={style}></Socials>
-            
+
             <li className={`${style} hover:scale-110`}>
               <button
-                className="pt-1" 
+                className="pt-1"
                 onClick={() => {
                   const mode = theme === "light" ? "dark" : "light";
                   setTheme(mode);
@@ -45,7 +48,6 @@ export default function Navbar() {
           </ul>
         </span>
       </nav>
-
     </div>
   );
 }
