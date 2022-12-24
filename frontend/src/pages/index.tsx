968ffb99c90e4432c6c43b97a09b72e2085726a7
Naqi Haider <naqihaider07@gmail.com>
2022-12-23 22:30:02 -0500

    Added Head tag

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from 'next/head'
 import Intro from "../components/intro/Intro";
 import Work from "../components/work/Work";
 import About from "../components/about/About";
@@ -7,6 +8,9 @@ import Navbar from "../components/navbar/Navbar";
 export default function Home() {
   return (
       <div className="flex flex-col gap-4 font-normal font-mono">
+        <Head>
+          <title>Naqi Haider</title>
+        </Head>
         <Navbar></Navbar>
         <Intro></Intro>
         <About></About>
